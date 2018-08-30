22f70f944c0e8b6f1c603cd43431de4ee5d84e26
Yenkis <jesusdbarajas@gmail.com>
2018-08-30 09:27:46 -0500

    Agregando la configuracion de sequelize

---
+++ libs/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    database: "task",
+    username: "",
+    password: "",
+    params: {
+        dialect: "postgres",
+        define: {
+            undercored: true
+        }
+    }
+}
