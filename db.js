dbf6fb6184b6de663ee50eabb094fa0f433151e5
Yenkis <jesusdbarajas@gmail.com>
2018-08-31 16:31:04 -0500

    Limpiesa y format el documento

--- db.js
+++ db.js
@@ -3,28 +3,8 @@ import path from "path";
 import Sequelize from "sequelize";
 import * as config from './libs/config';
 
-
 var db = {};
 
-
-
-console.log(config.database);
-console.log(config.username);
-console.log(config.password);
-console.log(config.host);
-console.log(config.port);
-console.log(config.schema);
-
-// const sequelize = new Sequelize({
-//     host: config.host,
-//     port: config.port,
-//     database: config.database,
-//     username: config.username,
-//     password: config.password,
-//     define: {
-//         schema: config.schema
-//     }
-// });
 var sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
     port: config.port,
@@ -33,10 +13,6 @@ var sequelize = new Sequelize(config.database, config.username, config.password,
         schema: config.schema
     }
 });
-
-
-// const sequelize = new Sequelize("postgres://larpjour:idIjzuU8a3vUqH-g6SSe9PFAlHdKHybb@pellefant.db.elephantsql.com:5432/larpjour");
-
 
 const dir = path.join(__dirname, "models");
 fs.readdirSync(dir).forEach(file => {
@@ -53,7 +29,6 @@ fs.readdirSync(dir).forEach(file => {
 Object.keys(db).forEach(key => {
     db[key].associate(db);
 });
-
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
--- libs/boot.js
+++ libs/boot.js
@@ -3,11 +3,11 @@ const db = require('../db');
 module.exports = app => {
     db.sequelize.sync().then(() => {
         console.log("fffkfkfkfkf");
-        
+
         app.listen(app.get("port"), () => {
             console.log(`Task API - Port ${app.get("port")}`);
         });
-    }).catch(err=>{
+    }).catch(err => {
         console.log(err);
     });
 };
